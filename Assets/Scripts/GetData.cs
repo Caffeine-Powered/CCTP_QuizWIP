bb6c858b8fbd971d23a5d77920f8767be2888a01
Caffeine-Powered <wmedwards96@gmail.com>
2026-03-23 02:27:55 +0000

    asdasdas

--- Assets/Scripts/GetData.cs
+++ Assets/Scripts/GetData.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 using SimpleJSON;
 using UnityEngine.Networking;
 using System.Collections;
+using TMPro;
 
 public class GetData : MonoBehaviour
 {
     public string DataURL;
     public int numOfQuestions;
-    public QuestionSetup questionSetup;
-    public AnswerButton answerButton;
+    public GameObject correctQText;
+    public GameObject[] questionInstances;
+    private int counter;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        counter = 0;
         StartCoroutine(getData());
         Debug.Log("Start");
     }
@@ -29,37 +33,46 @@ public class GetData : MonoBehaviour
             else
             {
                 string json = request.downloadHandler.text;
-                Debug.Log(json);
-                //JsonData = json; 
+                Debug.Log(json); 
                 ReadJSON(json);
-
-
-                questionSetup.GetQuestionAssets();
-                questionSetup.Cycle();
             }
         }
     }
 
-    void ReadJSON(string jsonString) //this inside of questiondata
+    void ReadJSON(string jsonString) 
     {
         JSONNode node = JSON.Parse(jsonString);
         JSONObject obj = node.AsObject;
     
-    /**    numOfQuestions = (obj["Questions"].Count);
-        
-
+        numOfQuestions = (obj["Questions"].Count);
 
         for (int i = 0 ; i < numOfQuestions; i++)
         {
-            Debug.Log(i);
+            //Debug.Log(i);
+  
+            Vector3 position = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(0.0f, 3.0f), Random.Range(-3.0f, 3.0f));
+            if (counter >= questionInstances.Length)
+            {
+                counter = 0;
+            }
+            Instantiate (questionInstances[counter], position, Random.rotation);
+            counter++;
+
+            string questionName = obj["Questions"][i]["Question"].Value;
+            string category = obj["Questions"][i]["Category"].Value;
+            string correctAnswer = obj["Questions"][i]["Correct Answer"].Value;
+            //string wrongAnswer1 = obj["Questions"][i]["Incorrect Answer 1"].Value;
+            //string wrongAnswer2 = obj["Questions"][i]["Incorrect Answer 2"].Value;
+            //string wrongAnswer3 = obj["Questions"][i]["Incorrect Answer 3"].Value;
+            //Debug.Log("correct + " +correctAnswer);
+            Debug.Log(correctAnswer);
+           
+
+            GameObject myText = Instantiate(correctQText, position + new Vector3(0, 0, 0), Quaternion.identity);
+            TextMeshPro textComponent = myText.GetComponent<TextMeshPro>();
+            textComponent.text = correctAnswer;
+            
 
         }
-        
-
-        //Debug.Log(obj["Questions"][0]);
-        //Debug.Log(obj["Questions"][1]);
-        //Debug.Log(obj["Questions"][2]);
-
-    **/
      }
 }
